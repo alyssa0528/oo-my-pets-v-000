0ec1c58036b35b44c600da425962a82fcd243f89
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 20:54:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -99,7 +99,7 @@ class Owner
   
   def list_pets
     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
-    binding.pry
+    #binding.pry
   end
    
 
